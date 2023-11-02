362e8f21b1003f3040624c85552d627353e6fef1
Rakhman Abdirashov <rakhmanabdirashov@Rakhmans-MacBook-Air.local>
2023-11-02 17:15:58 +0600

    add pagination

--- src/composables/useTwitterPosts.ts
+++ src/composables/useTwitterPosts.ts
@@ -1,21 +1,50 @@
 import {ITweets} from "@/types";
 import axios from "axios";
+import { onMounted, ref, watch } from "vue";
 
 const useTwitterPosts = () => {
+    const pageNumber = ref<number>(1);
+    const tweetPosts = ref<ITweets[]>([]);
+    let loading = false;
+
     const handleApiPost = async (endpoint: string) => {
+        loading = false;
         const result = await axios.get<{count: number; results: ITweets[]}>(endpoint, {
             headers: {
                 "Content-Type": "application/json",
             },
         });
-        return result.data.results;
+        loading = true;
+        return result.data.results as ITweets[];
     };
 
-    const fetchTweetPosts = async (): Promise<ITweets[]> => {
-        return await handleApiPost("https://api.nimbl.tv/ru/api/main/tweets/");
+    onMounted(async () => {
+        tweetPosts.value = await fetchTweetPostsByPageNumber(pageNumber.value);
+    });
+
+    watch(pageNumber, async () => {
+        const data = await fetchTweetPostsByPageNumber(pageNumber.value);
+        tweetPosts.value = [...tweetPosts.value, ...data]
+    })
+
+    const fetchTweetPostsByPageNumber = async (pageNumber: number): Promise<ITweets[]> => {
+        return await handleApiPost(`https://api.nimbl.tv/ru/api/main/tweets/?page=${pageNumber}`);
     };
 
-    return {fetchTweetPosts};
+    // const fetchTweetPosts = async (): Promise<ITweets[]> => {
+    //     return await handleApiPost(`https://api.nimbl.tv/ru/api/main/tweets/`);
+    // };
+
+    const fetchNextPosts = async () => {
+        if(loading == false) {
+            pageNumber.value += 1
+        }
+    }
+
+    return {
+        tweetPosts,
+        fetchNextPosts,
+    };
 };
 
 export default useTwitterPosts;
