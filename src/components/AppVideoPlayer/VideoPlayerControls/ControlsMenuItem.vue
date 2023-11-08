56fe9352773231bef4f11001198305acb4c19aeb
Eraly <thegunner125@gmail.com>
2023-11-08 18:15:00 +0600

    feat: new page tokenomics

--- src/components/AppVideoPlayer/VideoPlayerControls/ControlsMenuItem.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div class="px-4 py-2 whitespace-nowrap w-full flex flex-row gap-2 items-center hover:bg-emerald-600 cursor-pointer max-sm:px-2 max-sm:py-1" :tabindex="0">
-    <slot />
-  </div>
-</template>
