d444f61920a57cf7c8fc35c44b593eb081af1425
Eraly <thegunner125@gmail.com>
2023-12-11 15:13:30 +0600

    feat: add modal winers

--- src/components/InviteComponents/ModalWinners.vue
+++ src/components/InviteComponents/ModalWinners.vue
@@ -1,64 +1,63 @@
 <script setup lang="ts">
-import {onMounted, ref, PropType} from "vue";
+import {onMounted, ref} from "vue";
+import {useStorage} from "@vueuse/core";
 import axios, {AxiosError} from "axios";
 import gsap from "gsap";
+
 import {useAnimationDigits} from "@/composables/useAnimationDigits";
 import HeroButton from "@/components/HeroButton.vue";
 import IconsWinnerDone from "@/components/icons/IconsWinnerDone.vue";
+import IconsWinnerFail from "@/components/icons/IconsWinnerFail.vue";
+import {DEFAULT_USER_STORAGE, STORAGE_USER_KEY} from "@/constants";
+import {ISessionTwitter} from "@/types";
 
 const emit = defineEmits(["clickClose"]);
+
+const userStorage = useStorage<ISessionTwitter>(STORAGE_USER_KEY, DEFAULT_USER_STORAGE, sessionStorage);
 
 const containerEl = ref();
 const seasonWrapEl = ref();
 const buttonEl = ref();
-const activeSeasonEl = ref();
 const seasonTitleEl = ref();
 
 const isLoading = ref(false);
 const isEligible = ref(false);
 const nimbles = ref(0);
 const {tweened} = useAnimationDigits(() => nimbles.value);
+const currentSeason = ref(0);
+const passedSeason = ref(0);
 const errorText = ref<string | undefined>("");
 
 const clickHandle = async () => {
-    isLoading.value = true;
-
-    await new Promise((res): void => {
-        setTimeout(() => {
-            isEligible.value = true;
-            nimbles.value = 10043
-            return res(null);
-        }, 1500);
-    });
-
-    isLoading.value = false;
-};
-
-/* const onSubmit = async () => {
-    if (!sendCode.value.trim()) return;
-    if (!props.sessionToken) return;
     try {
-        const res = await axios.post(
-            "https://api.nimbl.tv/ru/api/hunter/ama/session/",
-            {
-                session_code: sendCode.value,
+        isLoading.value = true;
+        const res = await axios.get<{
+            username: string;
+            wallet_address: string;
+            token_quantity: number;
+            position: number;
+            season: number;
+            season_name: string;
+        }>("https://api.nimbl.tv/en/api/main/airdrop/0/season/", {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${userStorage.value.token}`,
             },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Token ${props.sessionToken}`,
-                },
-            },
-        );
+        });
         if (res.status === 200) {
-            emit('clickClose')
-        };
+            isEligible.value = true;
+            nimbles.value = res.data.token_quantity;
+        }
+        if (res.status === 404) {
+            errorText.value = 'You are not eligible'
+        }
     } catch (error) {
-        errorText.value = (error as AxiosError<{error: string}>).response?.data.error
+        errorText.value = (error as AxiosError<{error: string}>).response?.data.error;
+        errorText.value = 'You are not eligible'
+    } finally {
+        isLoading.value = false;
     }
-
-    
-}; */
+};
 
 onMounted(() => {
     const tl = gsap.timeline();
@@ -67,10 +66,10 @@ onMounted(() => {
             "25%": {
                 autoAlpha: 0.0,
             },
-            "50%": {
+            "45%": {
                 autoAlpha: 1.0,
             },
-            "75%": {
+            "60%": {
                 autoAlpha: 0.0,
             },
             "100%": {
@@ -78,13 +77,13 @@ onMounted(() => {
             },
         },
 
-        duration: 0.5,
+        duration: 0.6,
     });
     tl.from(
         containerEl.value,
         {
             height: 200,
-            duration: 1,
+            duration: 0.7,
             delay: 0.3,
         },
         "=-0",
@@ -93,23 +92,13 @@ onMounted(() => {
         seasonWrapEl.value.children,
         {
             autoAlpha: 0.0,
-            duration: 1,
+            duration: 0.8,
             yPercent: 100,
             delay: 0.3,
-            stagger: 0.3,
+            stagger: 0.2,
+            clearProps: true,
         },
-        "=-0.7",
-    );
-    tl.to(
-        activeSeasonEl.value,
-        {
-            autoAlpha: 1,
-            scale: 1.05,
-            duration: 0.5,
-            backgroundColor: 'yellow',
-            "--shadow": 1,
-        },
-        "=-0.4",
+        "=-0.6",
     );
     tl.from(
         seasonTitleEl.value,
@@ -118,16 +107,16 @@ onMounted(() => {
             scale: 0.8,
             yPercent: -100,
             duration: 0.7,
-            filter: "drop-shadow(0px -9px 2px #FFCD00)"
+            filter: "drop-shadow(0px -9px 2px #FFCD00)",
         },
-        "=-0.4",
+        "=-0.3",
     );
     tl.from(
         buttonEl.value,
         {
             autoAlpha: 0.0,
             yPercent: 100,
-            duration: 0.6,
+            duration: 0.5,
         },
         "=-0",
     );
@@ -140,39 +129,65 @@ onMounted(() => {
         class="fixed bg-black/50 w-screen h-screen isolate z-[99999] inset-x-0 inset-y-0 backdrop-blur-sm">
         <div
             ref="containerEl"
-            class="w_container absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[800px] h-[450px] max-md:h-[400px] max-sm:w-full max-sm:h-auto">
+            class="w_container will-change-[height] absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[800px] h-[450px] max-md:h-[400px] max-sm:w-full max-sm:h-auto">
+            <button class="w-12 h-12 absolute md:-right-12 md:-top-12 -right-3 -top-4 cursor-pointer z-20" @click="$emit('clickClose')">
+                <div class="md:w-9 w-6 bg-[#6842a4] h-1 rounded-md origin-center rotate-45 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md shadow-[#6842a4]" />
+                <div class="md:w-9 w-6 bg-[#6842a4] h-1 rounded-md origin-center -rotate-45 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md shadow-[#6842a4]" />
+            </button>
             <h3 class="text-[#CBAAFF] font-Rollbox font-black text-2xl text-center drop-shadow-[0px_3px_1px_#6842a4]">
                 RESULTS
             </h3>
 
             <div ref="seasonWrapEl" class="mt-10 flex justify-center">
-                <div class="wrap_season" ref="activeSeasonEl">
-                    <div class="season season_one active"><span>SEASON 1</span></div>
+                <div class="wrap_season" :class="{active: currentSeason === 0}" @click="currentSeason = 0">
+                    <div class="season season_one"><span>SEASON 1</span></div>
                 </div>
-                <div class="wrap_season">
-                    <div class="season season_two "><span>SEASON 2</span></div>
+                <div class="wrap_season" :class="{active: currentSeason === 1}" @click="currentSeason = 1">
+                    <div class="season season_two"><span>SEASON 2</span></div>
                 </div>
-                <div class="wrap_season">
-                    <div class="season season_three "><span>SEASON 3</span></div>
+                <div class="wrap_season" :class="{active: currentSeason === 2}" @click="currentSeason = 2">
+                    <div class="season season_three"><span>SEASON 3</span></div>
                 </div>
-                <div class="wrap_season">
-                    <div class="season season_four "><span>SEASON 4</span></div>
+                <div class="wrap_season" :class="{active: currentSeason === 3}" @click="currentSeason = 3">
+                    <div class="season season_four"><span>SEASON 4</span></div>
                 </div>
             </div>
 
-            <h2 ref="seasonTitleEl" class="mt-20 text-center font-Rollbox text-3xl font-black text-[#FFCD00] drop-shadow-[0px_-0px_2px_#FFCD00] max-md:text-2xl max-md:mt-14">SEASON ONE</h2>
+            <h2
+                ref="seasonTitleEl"
+                class="mt-20 text-center font-Rollbox text-3xl font-black text-[#FFCD00] drop-shadow-[0px_-0px_2px_#FFCD00] max-md:text-2xl max-md:mt-14">
+                <span v-if="currentSeason === 0">SEASON ONE</span>
+                <span v-if="currentSeason === 1">SEASON TWO</span>
+                <span v-if="currentSeason === 2">SEASON THREE</span>
+                <span v-if="currentSeason === 3">SEASON FOUR</span>
+            </h2>
 
-            <Transition mode="out-in" name="slide-up">
+            <Transition mode="out-in" name="slide-up"  ref="buttonEl">
+                <div v-if="!userStorage.user" class="text-center mt-7 text-2xl font-TTOctos font-bold text-blue-400">
+                    PLEASE LOG IN
+                </div>
+                <div v-else-if="passedSeason !== currentSeason" class="text-center mt-7 text-2xl font-TTOctos font-bold text-white/50">
+                    COMING SOON
+                </div>
                 <div
-                    v-if="isEligible"
+                    v-else-if="errorText"
                     class="font-TTOctos text-2xl max-md:text-xl text-center flex flex-col items-center mt-7 lg:mt-12 font-medium">
-                    <p class="text-[#DEFFA1]"><IconsWinnerDone class="w-6 h-6 inline max-md:w-4 max-md:h-4" /> You are eligibile</p>
+                    <p class="text-red-600">
+                        <IconsWinnerFail class="w-6 h-6 inline max-md:w-4 max-md:h-4" /> {{ errorText }}
+                    </p>
+                </div>
+                <div
+                    v-else-if="isEligible"
+                    class="font-TTOctos text-2xl max-md:text-xl text-center flex flex-col items-center mt-7 lg:mt-12 font-medium">
+                    <p class="text-[#DEFFA1]">
+                        <IconsWinnerDone class="w-6 h-6 inline max-md:w-4 max-md:h-4" /> You are eligible
+                    </p>
                     <p
-                        class="relative text-[#6D7CFF] after:bg-[url('/currency-Nimbl.png')] after:w-[16px] after:h-[16px] lg:after:w-[28px] lg:after:h-[26px] mt-2 after:inline-block after:bg-contain after:bg-no-repeat after:bg-center font-medium text-3xl max-md:text-2xl max-md:mt-0">
+                        class="relative text-[#6D7CFF] after:bg-[url('/currency-Nimbl.png')] after:w-[16px] after:h-[16px] lg:after:w-[28px] lg:after:h-[26px] mt-2 after:inline-block after:bg-contain after:bg-no-repeat after:bg-center text-3xl max-md:text-2xl max-md:mt-0 font-bold">
                         {{ tweened.number.toFixed(0) }}
                     </p>
                 </div>
-                <div v-else class="flex justify-center" ref="buttonEl">
+                <div v-else class="flex justify-center">
                     <div
                         class="hover:drop-shadow-[0px_4px_12px_#7030e8] transition-all mt-7 lg:mt-14 max-md:!w-[90%]"
                         :class="[isLoading && 'drop-shadow-[0px_4px_12px_#7030e8]']">
@@ -249,26 +264,31 @@ div.inner-border {
     filter: blur(16px);
 }
 .wrap_season {
-    --shadow: 0;
     position: relative;
     height: 50px;
     clip-path: polygon(15% 0%, 85% 0%, 100% 50%, 85% 100%, 15% 100%, 0% 50%);
     background-color: #6438ab;
     padding: 1px;
     opacity: 0.5;
-
-    @apply max-md:w-[140px] w-[150px]
+    cursor: pointer;
+    @apply max-md:w-[140px] w-[150px];
 }
-
+.wrap_season.active {
+    background-color: yellow;
+    opacity: 1;
+}
+.active.wrap_season::before {
+    opacity: 1;
+}
 .wrap_season::before {
-    content: '';
+    content: "";
     z-index: 2;
     position: absolute;
     width: 100%;
     height: 100%;
     inset: 0;
     display: block;
-    opacity: var(--shadow);
+    opacity: 0;
     box-shadow: inset 0px 0px 22px 14px #9cfd00ad;
 }
 
@@ -279,10 +299,7 @@ div.inner-border {
     display: flex;
     justify-content: center;
     align-items: center;
-    @apply m-[var(--b)] w-[calc(100%_-_2*var(--b))] h-[calc(100%_-_2*var(--b))] bg-gradient-header-secondary font-Rollbox font-bold max-md:text-[10px] max-md:font-medium
-    ;
-}
-.season_one.season {
+    @apply m-[var(--b)] w-[calc(100%_-_2*var(--b))] h-[calc(100%_-_2*var(--b))] bg-gradient-header-secondary font-Rollbox font-bold max-md:text-[10px] max-md:font-medium;
 }
 
 .wrap_season:nth-child(2),
--- src/views/BetaTestView.vue
+++ src/views/BetaTestView.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="relative w-full h-full flex flex-col max-sm:items-center">
-        <div ref="titleEl" data="titleEl" class="container justify-center flex pt-[50px] max-sm:pt-[20px]">
+        <div ref="titleEl" data="titleEl" class="container justify-center flex 2xl:pt-[50px] pt-[20px]">
             <div class="inline-flex flex-col items-center">
                 <div ref="secondTitleEl"
-                    class="py-2 px-4 border-[#6E7DFE] border bg-[#1C0048] inline-block lg:py-4 lg:px-6 font-gilroy font-medium tracking-[0.17em] lg:text-2xl mb-6 !leading-none">
+                    class="py-2 px-4 border-[#6E7DFE] border bg-[#1C0048] inline-block 2xl:py-4 md:py-3 2xl:px-6  font-gilroy font-medium tracking-[0.17em] 2xl:text-2xl md:text-lg mb-6 !leading-none">
                     BETA TESTING
                 </div>
                 <h1 class="main-title cursor-pointer"  @click="$router.push({name: 'nimbltv'})">
@@ -14,50 +14,50 @@
         </div>
 
         <div class="container flex flex-col lg:flex-row  grow pb-[35px] lg:gap-24 lg:mt-10 mt-0 overflow-y-clip">
-            <div class="lg:w-[500px] w-full self-start shrink-0 pt-5">
+            <div class="2xl:w-[500px] md:w-[400px] w-full self-start shrink-0 pt-5 max-md:pt-0">
                 <form ref="formEl" data="formEl" @submit.prevent="">
-                    <div class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 lg:py-5 lg:px-8">
+                    <div class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 2xl:py-5 2xl:px-8">
                         <input
                             @input="errorText = null"
                             type="text"
                             v-model="userName"
-                            class="bg-transparent font-gilroy outline-none w-full text-sm lg:text-2xl"
+                            class="bg-transparent font-gilroy outline-none w-full text-sm md:text-lg 2xl:text-2xl"
                             :class="[errorText ? 'text-red-500' : 'text-white']"
                             placeholder="Name/Pseudonym"
                             name="user" />
-                        <IconUser class="mr-5 lg:w-8 lg:h-8 w-5 h-5 text-white" />
+                        <IconUser class="mr-5 2xl:w-8 2xl:h-8 w-6 h-6 text-white" />
                         <div class="inner-border absolute inset-0"></div>
                     </div>
                     <div
-                        class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 lg:py-5 lg:px-8 mt-5 lg:mt-8">
+                        class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 2xl:py-5 2xl:px-8 mt-5 lg:mt-8">
                         <input
                             @input="errorText = null"
                             type="email"
                             v-model="userEmail"
-                            class="bg-transparent font-gilroy outline-none w-full text-sm lg:text-2xl"
+                            class="bg-transparent font-gilroy outline-none w-full text-sm md:text-lg 2xl:text-2xl"
                             :class="[errorText ? 'text-red-500' : 'text-white']"
                             placeholder="Email address"
                             name="email" />
-                        <IconEmail class="mr-5 lg:w-8 lg:h-8 w-5 h-5 text-white" />
+                        <IconEmail class="mr-5 2xl:w-8 2xl:h-8 w-5 h-5 text-white" />
                         <div class="inner-border absolute inset-0"></div>
                     </div>
                     <div
-                        class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 lg:py-5 lg:px-8 mt-5 lg:mt-8">
+                        class="relative flex flex-row-reverse items-center gap-0 px-5 py-3 2xl:py-5 2xl:px-8 mt-5 lg:mt-8">
                         <input
                             @input="errorText = null"
                             type="text"
                             v-model="userWallet"
-                            class="bg-transparent font-gilroy outline-none w-full text-sm lg:text-2xl"
+                            class="bg-transparent font-gilroy outline-none w-full text-sm md:text-lg 2xl:text-2xl"
                             :class="[errorText ? 'text-red-500' : 'text-white']"
                             placeholder="Wallet address"
                             name="wallet" />
-                        <IconWallet class="mr-5 lg:w-8 lg:h-8 w-5 h-5 fill-white" />
+                        <IconWallet class="mr-5 2xl:w-8 2xl:h-8 w-5 h-5 fill-white" />
                         <div class="inner-border absolute inset-0"></div>
                     </div>
 
-                    <HeroButton class="!w-full h-[55px] mt-8 lg:mt-14" title="coming soon">
-                        <p class="font-rfdewi text-2xl max-sm:text-sm font-bold tracking-widest">APPLY</p>
-                        <template #icon><IconFiveDots class="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" /></template>
+                    <HeroButton class="!w-full 2xl:!h-[75px] !h-[55px] mt-8 2xl:mt-14" title="coming soon">
+                        <p class="font-rfdewi text-2xl max-2xl:text-lg max-sm:text-sm font-bold tracking-widest">APPLY</p>
+                        <template #icon><IconFiveDots class="2xl:w-[30px] 2xl:h-[30px] w-[15px] h-[15px]" /></template>
                     </HeroButton>
 
                     <div class="flex justify-center items-center mt-5">
@@ -76,52 +76,52 @@
                     :src="imgPhone"
                     alt="imgPhone"
                     loading="lazy"
-                    class="lg:h-[125%] lg:!w-auto !max-w-[auto] w-[140px] object-contain shrink-0 relative z-20" />
+                    class="md:!h-[min(125%,_650px)] md:!w-auto !max-w-[auto] w-[140px] object-contain shrink-0 relative z-20" />
                 <div 
                     ref="descEl"
                     data="descEl"
-                    class="relative lg:w-[50vw] z-10 self-end before:absolute before:bottom-0 before:top-0 before:right-0 before:-left-[10%] before:bg-[#20133E]/50 before:hidden lg:before:block before:backdrop-blur-sm pt-10 isolate pl-5 flex flex-col justify-end">
-                    <div class="relative z-10">
-                        <p class="font-gilroy font-medium lg:text-2xl text-white">
+                    class="relative md:w-[60vw] z-10 self-stretch md:mt-8 before:absolute before:bottom-0 before:top-0 before:right-0 before:-left-[10%] before:bg-[#20133E]/50 before:hidden lg:before:block before:backdrop-blur-sm pt-10 isolate pl-5 flex flex-col justify-stretch">
+                    <div class="relative z-10 grow">
+                        <p class="font-gilroy font-medium 2xl:text-2xl md:text-xl text-white">
                             To apply for beta testing, you must have at least
                         </p>
-                        <p class="font-gilroy font-medium lg:text-2xl text-white">
+                        <p class="font-gilroy font-medium 2xl:text-2xl md:text-xl text-white">
                             <span
-                                class="text-xl lg:text-3xl font-bold text-[#6D7CFF] after:bg-[url('/currency-Nimbl.png')] after:w-[16px] after:h-[16px] lg:after:w-[24px] lg:after:h-[24px] after:inline-block after:bg-contain after:bg-no-repeat after:bg-center"
+                                class="text-xl 2xl:text-3xl md:text-2xl font-bold text-[#6D7CFF] after:bg-[url('/currency-Nimbl.png')] after:w-[16px] after:h-[16px] 2xl:after:w-[24px] 2xl:after:h-[24px] after:inline-block after:bg-contain after:bg-no-repeat after:bg-center"
                                 >5000</span
                             >
                             NIMBL tokens
                         </p>
                         <div class="border-b border-[#6D29A3] lg:my-5 my-2" />
-                        <p class="font-gilroy font-medium lg:text-2xl">in return you get:</p>
-                        <div ref="stacksEl" class="flex flex-col lg:flex-row gap-5 lg:mt-16 mt-4 lg:-mb-5">
+                        <p class="font-gilroy font-medium 2xl:text-2xl md:text-xl">in return you get:</p>
+                        <div ref="stacksEl" class="flex flex-col lg:flex-row gap-5 lg:mt-16 mt-4 lg:-mb-5 h-full">
                             <div
-                                class="relative bg-black/40 border border-[#0037ff]/50 lg:h-[280px] h-[50px]  w-[170px] px-6 flex justify-center items-center">
+                                class="relative bg-black/40 border border-[#0037ff]/50 md:h-[280px] 2xl:h-auto h-[45px]  w-[170px] px-6 flex justify-center md:pt-[100px] pt-[12px] items-start">
                                 <img
                                     loading="lazy"
                                     src="/beta-username.webp"
                                     alt="beta-username"
-                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2 -translate-y-1/2 [filter:_drop-shadow(-4px_-5px_8px_#f0b903)_drop-shadow(1px_6px_15px_#0037ff)] w-10 h-10 lg:w-[60%] lg:h-auto object-contain " />
-                                <p class="font-gilroy font-medium text-white lg:text-2xl text-sm text-center">Verified username</p>
+                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2 -translate-y-1/2 [filter:_drop-shadow(-4px_-5px_8px_#f0b903)_drop-shadow(1px_6px_15px_#0037ff)] w-10 h-10 2xl:w-[60%] md:w-[50%] md:h-auto object-contain " />
+                                <p class="font-gilroy font-medium text-white 2xl:text-2xl md:text-xl text-sm text-center">Verified username</p>
                             </div>
                             <div
-                                class="relative bg-black/40 border border-[#61f924]/40 lg:h-[280px] h-[50px] w-[170px] font-gilroy px-6 flex flex-col justify-center items-center">
+                                class="relative bg-black/40 border border-[#61f924]/40  md:h-[280px] 2xl:h-auto h-[45px] w-[170px] font-gilroy px-6 flex max-md:gap-1 md:flex-col justify-center md:justify-start md:pt-[100px] pt-[6px] items-center">
                                 <img
                                     loading="lazy"
                                     src="/beta-ultra.webp"
                                     alt="beta-username"
-                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2  -translate-y-1/2 drop-shadow-[0_4px_15px_#61f924] w-10 h-10 lg:w-[60%] lg:h-auto object-contain" />
-                                <p class="font-gilroy font-medium text-white lg:text-2xl text-sm text-center">Nimbl</p>
+                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2  -translate-y-1/2 drop-shadow-[0_4px_15px_#61f924] w-10 h-10 2xl:w-[60%] md:w-[50%] md:h-auto object-contain" />
+                                <p class="font-gilroy font-medium text-white 2xl:text-2xl md:text-xl text-sm text-center">Nimbl</p>
                                 <img src="/ultra-text.png" aria-hidden="true" class="h-3 lg:h-5 mt-0 lg:mt-2" />
                             </div>
                             <div
-                                class="relative bg-black/40 border border-[#9944e6]/40 lg:h-[280px] h-[50px] w-[170px] px-6 flex justify-center items-center">
+                                class="relative bg-black/40 border border-[#9944e6]/40  md:h-[280px] 2xl:h-auto h-[45px] w-[170px] px-6 flex justify-center md:pt-[100px] pt-[13px] items-start">
                                 <img
                                     loading="lazy"
                                     src="/beta-rating.webp"
                                     alt="beta-username"
-                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2  -translate-y-1/2 drop-shadow-[0_4px_15px_#9944e6] w-10 h-10 lg:w-[60%] lg:h-auto object-contain" />
-                                <p class="font-gilroy font-medium text-white lg:text-2xl text-sm text-center">Rating level 3</p>
+                                    class="absolute lg:top-0 top-1/2 lg:left-1/2 right-0 lg:-translate-x-1/2 translate-x-1/2  -translate-y-1/2 drop-shadow-[0_4px_15px_#9944e6] w-10 h-10 2xl:w-[60%] md:w-[50%] md:h-auto object-contain" />
+                                <p class="font-gilroy font-medium text-white 2xl:text-2xl md:text-xl text-sm text-center">Rating level 3</p>
                             </div>
                         </div>
                     </div>
@@ -271,7 +271,7 @@ onMounted(() => {
     background-image: -o-linear-gradient(180deg, rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.11) 100%);
     background-image: linear-gradient(180deg, rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.11) 100%);
     position: relative;
-    @apply text-center font-rfdewi inline-block bg-clip-text  text-[125px] max-sm:text-[55px] !leading-none font-black uppercase -translate-x-2 -translate-y-7 max-sm:-translate-x-0 max-sm:-translate-y-6;
+    @apply text-center font-rfdewi inline-block bg-clip-text  text-[125px] max-2xl:text-[80px] max-sm:text-[55px] !leading-none font-black uppercase -translate-x-2 -translate-y-7 max-sm:-translate-x-0 max-sm:-translate-y-6;
     -webkit-font-smoothing: antialiased;
     text-shadow: none;
     color: transparent;
@@ -289,6 +289,7 @@ onMounted(() => {
     background-color: rgb(56, 123, 255);
     text-shadow: none;
     color: transparent;
+    font-size: 1em;
 }
 @supports (background-clip: text) and (-webkit-text-fill-color: transparent) {
     .main-title {
